9b25eff72cc933554bd49bb61e05303126e5abf3
JeremyPerezSwift <jeremyperez34000@gmail.com>
2023-08-10 15:22:09 +0200

    Start of authentification

---
+++ ChatApp/Controller/Authentification/RegistrationController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationController.swift
+//  ChatApp
+//
+//  Created by Jérémy Perez on 10/08/2023.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selector
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .systemPink
+    }
+    
+}
