661b620da3cec3dc7c95d7ee186dfa0e1d6ce596
JeremyPerezSwift <jeremyperez34000@gmail.com>
2023-08-10 08:13:29 +0200

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Jérémy Perez on 10/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
